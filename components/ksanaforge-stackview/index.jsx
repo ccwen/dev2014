8dea633c99907ce661ba68383949850cf1742ce0
yapcheahshen <yapcheahshen@gmail.com>
2014-09-20 23:16:19 +0800

    animated selected text

--- components/ksanaforge-stackview/index.jsx
+++ components/ksanaforge-stackview/index.jsx
@@ -7,12 +7,17 @@ var stackview = React.createClass({
   },
   createSplitter:function() {
     var mainheight=$(this.getDOMNode()).height();
+    var mainwidth=$(this.getDOMNode()).width();
+
     for (var i=0;i<this.props.views.length-1;i++) {
       var container=$(this.refs["v"+i].getDOMNode());
       var h=$(container.children()[0]).height();
-      if (i==0) container.height(mainheight+20);
+      if (i==0) {
+        container.height(mainheight);
+        container.width(mainwidth);
+      }
       container.splitter(
-        {type:'h', outline:true, sizeTop: h}
+        {type:'h',  sizeTop: h+5} //outline:true to avoid flicker
       );      
     }
   },
@@ -20,22 +25,22 @@ var stackview = React.createClass({
     this.start=0;
   },
   componentDidMount:function() {
-    setTimeout(this.createSplitter.bind(this),50);
+    setTimeout(this.createSplitter.bind(this),100);
   },
   componentDidUpdate:function() {
     
-  },   
+  },
   renderView:function(v) {
-    return <div className="splitterPane">{v.content}</div>
+    return <div className="splitterPane" dangerouslySetInnerHTML={{__html:v.content}}></div>
   },
   createNestedView:function() {
     if (this.start>=this.props.views.length) return null;
     return React.DOM.div(
-      {ref:"v"+this.start},
+      {ref:"v"+this.start}, 
       this.renderView(this.props.views[this.start++]),
-      this.createNestedView()
-    );
-  },
+      this.createNestedView() 
+    );       
+  },  
   render: function() {
     return <div className="stackview">
       {this.createNestedView()}
